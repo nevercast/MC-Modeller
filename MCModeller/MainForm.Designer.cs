505a99a98c38fe77ab76342707e74099cc17d7a0
NeverCast <nevercast@nevercast.net>
2012-10-18 19:41:19 +1300

    Testing SharpGL

---
+++ MCModeller/MainForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace MCModeller
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openGLViewport = new SharpGL.OpenGLControl();
+            this.panelLeftPane = new System.Windows.Forms.Panel();
+            this.panelRightPane = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLViewport)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLViewport
+            // 
+            this.openGLViewport.BackColor = System.Drawing.Color.Black;
+            this.openGLViewport.BitDepth = 24;
+            this.openGLViewport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.openGLViewport.DrawFPS = true;
+            this.openGLViewport.FrameRate = 20;
+            this.openGLViewport.Location = new System.Drawing.Point(200, 0);
+            this.openGLViewport.Name = "openGLViewport";
+            this.openGLViewport.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLViewport.Size = new System.Drawing.Size(469, 557);
+            this.openGLViewport.TabIndex = 0;
+            this.openGLViewport.OpenGLInitialized += new System.EventHandler(this.openGLViewport_OpenGLInitialized);
+            this.openGLViewport.OpenGLDraw += new System.Windows.Forms.PaintEventHandler(this.openGLViewport_OpenGLDraw);
+            this.openGLViewport.Resized += new System.EventHandler(this.openGLViewport_Resized);
+            // 
+            // panelLeftPane
+            // 
+            this.panelLeftPane.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeftPane.Location = new System.Drawing.Point(0, 0);
+            this.panelLeftPane.Name = "panelLeftPane";
+            this.panelLeftPane.Size = new System.Drawing.Size(200, 557);
+            this.panelLeftPane.TabIndex = 1;
+            // 
+            // panelRightPane
+            // 
+            this.panelRightPane.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelRightPane.Location = new System.Drawing.Point(669, 0);
+            this.panelRightPane.Name = "panelRightPane";
+            this.panelRightPane.Size = new System.Drawing.Size(200, 557);
+            this.panelRightPane.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 557);
+            this.Controls.Add(this.openGLViewport);
+            this.Controls.Add(this.panelRightPane);
+            this.Controls.Add(this.panelLeftPane);
+            this.Name = "MainForm";
+            this.Text = "MC Modeller";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLViewport)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLViewport;
+        private System.Windows.Forms.Panel panelLeftPane;
+        private System.Windows.Forms.Panel panelRightPane;
+    }
+}
+
---
+++ MCModeller/MainForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+
+namespace MCModeller
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #region OpenGL Context
+        private OpenGL __openGLContext;
+        public OpenGL GL
+        {
+            get
+            {
+                if (__openGLContext == null)
+                    __openGLContext = openGLViewport.OpenGL;
+                return __openGLContext;
+            }
+        }
+
+        #endregion
+
+        float rotation;
+
+        private void openGLViewport_OpenGLDraw(object sender, PaintEventArgs e)
+        {
+            //  Clear the color and depth buffer.
+            GL.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            //  Load the identity matrix.
+            GL.LoadIdentity();
+
+            //  Rotate around the Y axis.
+            GL.Rotate(rotation, 0.0f, 1.0f, 0.0f);
+
+            //  Draw a coloured pyramid.
+            GL.Begin(OpenGL.GL_TRIANGLES);
+            GL.Color(1.0f, 0.0f, 0.0f);
+            GL.Vertex(0.0f, 1.0f, 0.0f);
+            GL.Color(0.0f, 1.0f, 0.0f);
+            GL.Vertex(-1.0f, -1.0f, 1.0f);
+            GL.Color(0.0f, 0.0f, 1.0f);
+            GL.Vertex(1.0f, -1.0f, 1.0f);
+            GL.Color(1.0f, 0.0f, 0.0f);
+            GL.Vertex(0.0f, 1.0f, 0.0f);
+            GL.Color(0.0f, 0.0f, 1.0f);
+            GL.Vertex(1.0f, -1.0f, 1.0f);
+            GL.Color(0.0f, 1.0f, 0.0f);
+            GL.Vertex(1.0f, -1.0f, -1.0f);
+            GL.Color(1.0f, 0.0f, 0.0f);
+            GL.Vertex(0.0f, 1.0f, 0.0f);
+            GL.Color(0.0f, 1.0f, 0.0f);
+            GL.Vertex(1.0f, -1.0f, -1.0f);
+            GL.Color(0.0f, 0.0f, 1.0f);
+            GL.Vertex(-1.0f, -1.0f, -1.0f);
+            GL.Color(1.0f, 0.0f, 0.0f);
+            GL.Vertex(0.0f, 1.0f, 0.0f);
+            GL.Color(0.0f, 0.0f, 1.0f);
+            GL.Vertex(-1.0f, -1.0f, -1.0f);
+            GL.Color(0.0f, 1.0f, 0.0f);
+            GL.Vertex(-1.0f, -1.0f, 1.0f);
+            GL.End();
+
+            //  Nudge the rotation.
+            rotation += 3.0f;
+        }
+
+        private void openGLViewport_Resized(object sender, EventArgs e)
+        {
+            //  Set the projection matrix.
+            GL.MatrixMode(OpenGL.GL_PROJECTION);
+
+            //  Load the identity.
+            GL.LoadIdentity();
+
+            //  Create a perspective transformation.
+            GL.Perspective(60.0f, (double)Width / (double)Height, 0.01, 100.0);
+
+            //  Use the 'look at' helper function to position and aim the camera.
+            GL.LookAt(-5, 5, -5, 0, 0, 0, 0, 1, 0);
+
+            //  Set the modelview matrix.
+            GL.MatrixMode(OpenGL.GL_MODELVIEW);
+        }
+
+        private void openGLViewport_OpenGLInitialized(object sender, EventArgs e)
+        {
+            //  Set the clear color.
+            GL.ClearColor(0, 0, 0, 0);
+        }
+    }
+}
